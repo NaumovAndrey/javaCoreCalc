130c4a1e284d1b4fea0987fe69aa517d3ccf80b3
andrey <naumov.a.n@icloud.com>
2023-09-05 22:26:55 +0700

    input ViewConsole

--- src/view/ViewConsole.java
+++ src/view/ViewConsole.java
@@ -1,14 +1,8 @@
 package view;
 
-import java.util.Scanner;
-
 public class ViewConsole {
-    Scanner scanner = new Scanner(System.in);
 
-    public String  dataRequest(){
-        System.out.print("Введите выражение: ");
-        String line = scanner.nextLine();
+    public void dataRequest() {
 
-        return line;
     }
 }
